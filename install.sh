05f62a01c23ac8a61217028e170def741fcf3986
Antonio Cicero Azevedo <azevedoantoniocicero@gmail.com>
2024-10-25 19:33:39 -0300

    resolvido

--- install.sh
+++ install.sh
@@ -2,11 +2,13 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt install -y git
+sudo apt install -y git vim
 
 mkdir apps
 cd apps
 
-curl -O https://www.python.org/ftp/python/3.12.7/Python-3.12.7.tgz
-tar -xf Python-3.12.7.tgz
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+sudo snap install docker
+sudo snap install --classic kotlin
 
